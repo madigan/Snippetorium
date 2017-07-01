ce91f5f56615936824d17904daf166972d929a21
John Lynn <john@silferein.com>
2017-07-01 13:20:26 -0700

    Add Gosu Text example

---
+++ Gosu/text-example/text-example.rb
@@ -0,0 +1,16 @@
+require 'gosu'
+
+class TextExample < Gosu::Window
+	def initialize
+		super 640, 480
+		self.caption = "Text Example"
+
+		@font = Gosu::Font.new(32, name: "Nimbus Mono L")
+    end
+
+	def draw
+		@font.draw("Otter.Tech Rocks My Socks!", 10, 20, 0)
+	end
+end
+
+window = ImageExample.new.show
